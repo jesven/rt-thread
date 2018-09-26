155e32fdcba5a2e8d35f3f830b3db65410ef8033
shaojinchun <shaojinchun@rt-thread.com>
2018-09-26 11:07:27 +0800

    修改了main中的hello rt-thread显示，使之不再和msh>交错

--- bsp/zynq7000_smp/applications/main.c
+++ bsp/zynq7000_smp/applications/main.c
@@ -1,10 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "rtthread.h"
+#include "rthw.h"
 
 int main(void)
 {
-    printf("hello rt-thread\n");
+    rt_uint32_t level;
+
+    level = rt_hw_interrupt_disable();
+    rt_kprintf("hello rt-thread\n");
+    rt_hw_interrupt_enable(level);
 
     return 0;
 }
